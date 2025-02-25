7bb08590a0e06372e08a7749fd36b4d71ac1df72
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-02-25 11:20:59 +0900

    스프링 DB 접근 기술

--- src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -19,37 +19,46 @@ class MemoryMemberRepositoryTest {
 
     @Test
     public void save() {
+        //given
         Member member = new Member();
         member.setName("spring");
 
+        //when
         repository.save(member);
 
+        //then
         Member result = repository.findById(member.getId()).get();
         assertThat(result).isEqualTo(member);
     }
 
     @Test
     public void findByName() {
+        //given
         final Member member1 = new Member();
         member1.setName("spring1");
+
+        //when
         repository.save(member1);
 
+        //then
         final Member result = repository.findByName("spring1").get();
         assertThat(result).isEqualTo(member1);
     }
 
     @Test
     public void findAll() {
+        //given
         final Member member1 = new Member();
         member1.setName("spring1");
-        repository.save(member1);
-
         final Member member2 = new Member();
         member2.setName("spring2");
+
+        //when
+        repository.save(member1);
         repository.save(member2);
 
+        //then
         final List<Member> result = repository.findAll();
-
         assertThat(result.size()).isEqualTo(2);
     }
 
---
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,58 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        final Member member = new Member();
+        member.setName("spring");
+
+        //when
+        final Long saveId = memberService.join(member);
+
+        //then
+        final Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        final Member member1 = new Member();
+        member1.setName("spring");
+
+        final Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        final IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+//        try {
+//            memberService.join(member2);
+//            fail("예외가 발생해야 합니다.");
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+    }
+}
