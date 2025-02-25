d08532447b6613923da7f505c5d4aac1a0bfb6ce
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-02-25 15:04:08 +0900

    AOP

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,11 @@
 package hello.hello_spring;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -23,7 +21,6 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
-
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
